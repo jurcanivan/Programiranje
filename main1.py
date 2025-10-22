68e8a77aefd06edfc6379157991eb724bd09d6ae
jurcanivan <ivan.jurcan1@skole.hr>
2025-10-22 23:25:18 +0200

    Update main1.py

--- main1.py
+++ main1.py
@@ -1,88 +1,80 @@
-STOP_WORDS = ['i', 'u', 'na', 'je', 'se', 'su', 's', 'za', 'o', 'a', 'pa', 'te', 'li', 'da', 'ali', 'bi', 'bio', 'bila', 'što', 'ga', 'mu', 'joj', 'ih']
-#ucitavamo tekst
-def ucitaj_tekst(filepath):
-    try:
-    #ovdje ide logika za čitanje datoteke
-        with open (filepath,'r',encoding='utf-8') as file:
-         sadrzaj = file.read()
-        return sadrzaj
-    except FileNotFoundError:
-        print(f'greška: Datoteka na putanji {filepath} ne postoji.')
-        return None #vraća prazan skup podataka, ako ne nađe datoteku
+STOP_WORDS = ['i', 'u', 'na', 'je', 'se', 'su', 's', 'za', 'o', 'a', 'pa', 'te', 'li', 'da', 'ali', 'bi', 'bilo', 'što', 'ga', 'mu', 'joj', 'ih']
 
-#funkcija koja pročišćava tekst
+# Funkcija koja učitava tekst iz datoteke
+def ucitaj_tekst(filepath):  
+    """Učitava tekst iz zadane datoteke i vraća ga kao string."""  
+    try:  
+        with open(filepath, 'r', encoding='utf-8') as file:  # otvaramo datoteku za čitanje u UTF-8 formatu
+            return file.read()  # čitamo cijeli sadržaj datoteke
+    except FileNotFoundError:  # ako datoteka ne postoji
+        print(f'Greška: Datoteka "{filepath}" ne postoji.')  # ispisujemo poruku o grešci
+        return None  # vraćamo None da signaliziramo grešku
 
-def ocisti_tekst(tekst):
-    #ovdje ide logika za pročišćavanje teksta❤
-    tekst = tekst.lower()
-    interpunkcija = ['.', ',', '!', '?', ':', ';', '"', "'", '(', ')']    
-    for znak in interpunkcija:
-        tekst = tekst.replace(znak, '')
-    lista_rijeci = tekst.split()
+# Funkcija koja čisti tekst od interpunkcije i pretvara u mala slova
+def ocisti_tekst(tekst):  
+    """Pretvara tekst u mala slova i uklanja interpunkciju."""  
+    tekst = tekst.lower()  # pretvaramo sve znakove u mala slova
+    interpunkcija = ['.', ',', '!', '?', ':', ';', '"', "'", '(', ')', '-', '_', '–']  # znakovi koje želimo ukloniti
+    for znak in interpunkcija:  # prolazimo kroz sve znakove
+        tekst = tekst.replace(znak, '')  # brišemo ih iz teksta
+    return tekst.split()  # razdvajamo riječi i vraćamo listu riječi
 
-    return lista_rijeci
+# Funkcija koja broji koliko se puta svaka riječ pojavljuje
+def broji_frekvenciju(lista_rijeci):  
+    """Vraća rječnik s riječima i njihovom frekvencijom."""  
+    rjecnik_frekvencija = {}  # kreiramo prazan rječnik
+    for rijec in lista_rijeci:  # prolazimo kroz svaku riječ
+        rjecnik_frekvencija[rijec] = rjecnik_frekvencija.get(rijec, 0) + 1  # povećavamo broj pojavljivanja
+    return rjecnik_frekvencija  # vraćamo rječnik s rezultatima
 
-#koliko se puta ponavlja ista rijec
-def broji_frekvenciju(lista_rijeci):
-    # Kreiramo prazam skup di skupljamo rezultate
-    rjecnik_frekvencija = {}
+# Funkcija koja uklanja "stop-words" iz rječnika frekvencija
+def ukloni_stop_words(rjecnik_frekvencija, stop_words_lista):  
+    """Uklanja nebitne riječi (stop-words) iz rječnika frekvencija."""  
+    ocisceni_rjecnik = {}  # stvaramo novi rječnik za filtrirane riječi
+    for rijec, frekvencija in rjecnik_frekvencija.items():  # prolazimo kroz sve parove (riječ, frekvencija)
+        if rijec not in stop_words_lista:  # ako riječ nije u listi stop-words
+            ocisceni_rjecnik[rijec] = frekvencija  # dodajemo je u novi rječnik
+    return ocisceni_rjecnik  # vraćamo očišćeni rječnik
 
-    # Prolazimo kroz svaku rijec u primljenoj listi
-    for rijec in lista_rijeci:
-        if rijec in rjecnik_frekvencija:
-            rjecnik_frekvencija[rijec] += 1
-        else: 
-            rjecnik_frekvencija[rijec] = 1
-    return rjecnik_frekvencija
+# Funkcija koja sortira rječnik po frekvenciji i ispisuje najčešće riječi
+def sortiraj_i_ispisi(rjecnik_frekvencija, broj_rijeci=15):  
+    """
+    Sortira rječnik po frekvenciji (od najveće prema najmanjoj)
+    i ispisuje top 'broj_rijeci' rezultata.
+    """  
+    # pretvaramo rječnik u listu parova i sortiramo po vrijednosti (frekvenciji)
+    sortirana_lista = sorted(rjecnik_frekvencija.items(), key=lambda item: item[1], reverse=True)  
 
-#Čišćenje teksta od veznika i sličnih "nebitnih" riječi
-def ukloni_stop_works(rjecnik_frekvencija, stop_words_lista):
-    ocisceni_rjecnik = {}
-    for rijec, frekvencija in rjecnik_frekvencija.items():
-        if rijec not in stop_words_lista:
-            ocisceni_rjecnik[rijec] = frekvencija
-        return ocisceni_rjecnik
+    print("\n--- Top", broj_rijeci, "najčešćih riječi ---")  # ispis zaglavlja
+    for i, (rijec, frekvencija) in enumerate(sortirana_lista[:broj_rijeci]):  # prolazimo kroz prvih N riječi
+        print(f"{i+1}. {rijec}: {frekvencija}")  # ispisujemo redni broj, riječ i broj ponavljanja
+    print("-" * 40)  # ispis linije razdjelnika
 
+# Glavni dio programa – pokreće se samo ako se datoteka izvršava direktno
+if __name__ == '__main__':  
+    filepath = 'tekst.txt'  # naziv datoteke iz koje čitamo tekst
+    print(f'Učitavam tekst iz datoteke: {filepath}')  # informacija korisniku
 
+    ucitani_tekst = ucitaj_tekst(filepath)  # poziv funkcije za učitavanje teksta
+    if not ucitani_tekst:  # ako nije učitano (None ili prazan string)
+        print('Program se prekida jer tekst nije učitan.')  # ispis poruke
+        exit()  # prekid programa
 
-#glavni dio programa
-if __name__ == '__main__':
-    filepath = 'tekst.txt'
-    print(f'učitavam tekst iz datoteke :{filepath}')
+    print('\nTekst uspješno učitan!')  # potvrda da je sve u redu
+    print('-' * 40)  # ispis crte
+    print(ucitani_tekst)  # ispis učitanog sadržaja
+    print('-' * 40)  # ispis crte
 
-    ucitani_tekst = ucitaj_tekst(filepath)
+    procisceni_tekst = ocisti_tekst(ucitani_tekst)  # poziv funkcije za čišćenje teksta
+    print("\nPročišćeni tekst:")  # naslov
+    print(procisceni_tekst)  # ispis liste riječi nakon čišćenja
 
-    if ucitani_tekst:
-        print('\ntekst uspješno učitan. slijedi ispis sadržaja:')
-        print('-' * 40)
-        print(ucitani_tekst)
-        print('-' * 40)
-    else:
-        print('program se prekida jer tekst nije učitan.')
+    broj_rijeci = broji_frekvenciju(procisceni_tekst)  # brojimo pojavljivanja riječi
+    print("\n--- Rječnik frekvencija ---")  # naslov
+    print(broj_rijeci)  # ispis rječnika s rezultatima
 
-    procisceni_tekst = ocisti_tekst(ucitani_tekst)
+    ociscene_frekvencije = ukloni_stop_words(broj_rijeci, STOP_WORDS)  # uklanjamo stop-words
+    print("\n--- Očišćeni rječnik frekvencija ---")  # naslov
+    print(ociscene_frekvencije)  # ispis rječnika bez nevažnih riječi
 
-    if procisceni_tekst:
-        print("\nProcisceni tekst je: ")
-        print('-' * 40)
-        print(procisceni_tekst)
-        print('-' * 40)
-    
-        #Brojimo rijeci za frekvenciju
-        print("Brojim frekvenciju riječi...")
-        broj_rijeci = broji_frekvenciju(procisceni_tekst)
-        print("Brojanje završeno!")
-
-        #Ispisujemo rezultat da vidimo što smo dobili
-        print("\n--- Rječnik frekvencija ---")
-        print(broj_rijeci)
-        print("-" * 40)
-    
-        ociscene_frekvencije = ukloni_stop_works(broj_rijeci, STOP_WORDS)
-        print("\n--- Očišćeni rječnik frekvencija ---")
-        print(ociscene_frekvencije)
-        print("-" * 40)
-
-
-
-
+    sortiraj_i_ispisi(ociscene_frekvencije, broj_rijeci=15)
